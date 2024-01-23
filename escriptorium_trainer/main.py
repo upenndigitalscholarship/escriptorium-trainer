7c353e323e113f49101a67da01599db71c59e63f
apjanco <apjanco@gmail.com>
2024-01-23 12:38:54 -0500

    training

--- escriptorium_trainer/main.py
+++ escriptorium_trainer/main.py
@@ -8,12 +8,106 @@ from PIL import Image
 import getpass
 import requests 
 from kraken.lib import models
+from kraken.lib.default_specs import RECOGNITION_HYPER_PARAMS
+from kraken.lib.train import KrakenTrainer, RecognitionModel
+from kraken.ketos.util import to_ptl_device
+import torch
+from datetime import date
 from zipfile import ZipFile
 from io import BytesIO
 import io
 
 app = typer.Typer()
 
+
+def make_training_data(E: EscriptoriumConnector, documents:list, transcription_pk:int, training_data_path: Path):
+    # helper funtion to create training and evaluation data
+    # training_data = [{'image': os.path.join(settings.MEDIA_ROOT, lt['image']),
+    #                       'text': lt['content'],
+    #                       'baseline': lt['baseline'],
+    #                       'boundary': lt['mask']} for lt in ground_truth[partition:]]
+
+    # evaluation_data = [{'image': os.path.join(settings.MEDIA_ROOT, lt['image']),
+    training_data = []
+
+    for document in documents:
+        parts = E.get_document_parts(document.pk)
+        for part in track(parts.results, description=f"Downloading {document.name}..."):
+            if not (training_data_path / f"{part.filename}").exists():
+                img_binary = E.get_document_part_image(document.pk, part.pk)
+                img = Image.open(io.BytesIO(img_binary))
+                # save image
+                img.save(str(training_data_path / f"{part.filename}"))
+            image_path = str(training_data_path / f"{part.filename}")
+            # get lines
+            lines = E.get_document_part_lines(document.pk, part.pk) 
+            #get baseline and mask
+            for line in lines.results: 
+                baseline = line.baseline
+                boundary = line.mask
+                
+                # get text transcription
+                part_line_transcription = E.get_document_part_line_transcription_by_transcription(document.pk, part.pk,line.pk,transcription_pk) 
+                if part_line_transcription:
+                    #ignore if content is None or empty
+                    if part_line_transcription.content is None or part_line_transcription.content == "":
+                        continue
+                    else:
+                        text = part_line_transcription.content
+                        training_data.append({'image': image_path,
+                                            'text': text,
+                                            'baseline': baseline,
+                                            'boundary': boundary})
+    return training_data
+
+
+def train(training_data, model_dir='models/HTR-Araucania_XIX.mlmodel', models_path:Path=Path('models'), model_name:str='new_model'):
+    # helper function to train a new model
+    # model_dir = path to existing model for fine-tuning
+    # based on https://gitlab.com/scripta/escriptorium/-/blob/develop/app/apps/core/tasks.py?ref_type=heads#L418
+    partition = int(len(training_data) / 10)
+
+    training_data = [d for d in training_data[partition:]]
+    evaluation_data = [d for d in training_data[:partition]]
+
+    load = model_dir
+    reorder = 'R' # for RTL languages else 'L'
+    if torch.cuda.is_available():
+        device = 'cuda:0'
+    else:
+        device = 'cpu'
+    accelerator, device = to_ptl_device(device)
+
+    kraken_model = RecognitionModel(hyper_params=RECOGNITION_HYPER_PARAMS,
+                                    output=str(models_path) + "/" + model_name,
+                                    model=load,
+                                    reorder=reorder,
+                                    format_type=None,
+                                    training_data=training_data,
+                                    evaluation_data=evaluation_data,
+                                    partition=partition,
+                                    num_workers=4,
+                                    load_hyper_parameters=True,
+                                    repolygonize=False,
+                                    resize='add')
+
+    trainer = KrakenTrainer(accelerator=accelerator,
+                            devices=device,
+                            # max_epochs=,
+                            # min_epochs=hyper_params['min_epochs'],
+                            enable_progress_bar=False,
+                            val_check_interval=1.0,
+                            # deterministic=ctx.meta['deterministic'],
+                            #callbacks=[FrontendFeedback(model, model_dir, document.pk)]
+                            )
+    trainer.fit(kraken_model)
+
+    if kraken_model.best_epoch == -1:
+        print('Model did not converge, please try again with more data.')
+    else:
+        best_version = str(models_path) + "/" + kraken_model.best_model
+        print(kraken_model.best_metric)
+        return best_version
 
 # add optional argument for --clear-secrets
 @app.callback(invoke_without_command=True)
@@ -68,36 +162,48 @@ def main(clear_secrets: bool = typer.Option(False), fine_tune: bool = typer.Opti
     else:
         project_slug = None
 
+    # create folder for training data
+    training_data_path = Path("training_data")
+    if not training_data_path.exists():
+        training_data_path.mkdir(parents=True,exist_ok=True)
+    # delete files in training_data folder
+    #for file in training_data_path.iterdir():
+    #    file.unlink()
+
+    # create folder for models
+    models_path = Path("models")
+    if not models_path.exists():
+        models_path.mkdir(parents=True,exist_ok=True)
+
     # fetch all project images
     all_documents = E.get_documents()
     documents = [doc for doc in all_documents.results if doc.project == project_slug]
-    for document in documents:
-        transcriptions = E.get_document_transcriptions(document.pk)
-        manual = [t for t in transcriptions if t.name == "manual"]
-        if manual:
-            transcription_pk = manual[0].pk
-
-        parts = E.get_document_parts(document.pk)
-        for part in track(parts.results, description=f"Downloading {document.name}..."):
-            img_binary = E.get_document_part_image(document.pk, part.pk)
-            img = Image.open(io.BytesIO(img_binary))
-            alto_xml = E.download_part_alto_transcription(
-                document.pk, part.pk, transcription_pk
+    #select transcription text to train on IMPORTANT!
+    if documents:
+        transcriptions = E.get_document_transcriptions(documents[0].pk)
+        transcription_names = [t.name for t in transcriptions]
+        for i, name in enumerate(transcription_names):
+            print(
+                f"[bold green_yellow]{i}[/bold green_yellow] [bold white]{name}[/bold white]"
             )
-            # You will need to unzip these bytes in order to access the XML data (zipfile can do this).
-            with ZipFile(BytesIO(alto_xml)) as z:
-                with z.open(z.namelist()[0]) as f:
-                    alto_xml = f.read()
-            # create folder for training data
-            training_data_path = Path("training_data")
-            if not training_data_path.exists():
-                training_data_path.mkdir(parents=True,exist_ok=True)
-            # save image
-            img.save(training_data_path / f"{document.name}_{part.name}.png")
-            # save alto xml
-            with open(training_data_path / f"{document.name}_{part.name}.xml", "wb") as f:
-                f.write(alto_xml)
-        
+        selection = typer.prompt("Please select a transcription text to train on")
+        # if the user enters a number, use that to select the document
+        if selection.isdigit():
+            transcription_pk = transcriptions[int(selection)].pk
+            transcription_name = transcriptions[int(selection)].name
+            print(
+                f"[bold green_yellow] 🏋️ Using text from {transcription_name}...[/bold green_yellow]"
+            )
+        else:
+            print("Please enter a number to select the transcription text to train on")
+
+        training_data = make_training_data(E, documents,transcription_pk, training_data_path)
+        srsly.write_json(training_data_path / "training_data.json", training_data)
+
+
+    else:
+        print("No documents found in this project")
+
     # train a new model or fine-tune an existing one
     if fine_tune:
         e_url = secrets["ESCRIPTORIUM_URL"]
@@ -137,10 +243,18 @@ def main(clear_secrets: bool = typer.Option(False), fine_tune: bool = typer.Opti
                 )
         else:
             print(f"[bold red]Error {models.raise_for_status()}[/bold red]")
+    
+    if fine_tune:
+        model = train(training_data, model_dir=rec_model_path)
+    else:
+        model = train(training_data, model_name=f'new_model-{date.today()}')
+        print(model)
     # choose projects to use for training 
     # fetch data
     # train model
     # push model to escriptorium
+
+
 
 if __name__ == "__main__":
     app()
--- escriptorium_trainer/notes.md
+++ escriptorium_trainer/notes.md
@@ -1,4 +1,98 @@
+# WOrk from blog
+https://digitalorientalist.com/2023/09/26/train-your-own-ocr-htr-models-with-kraken-part-1/
+find training_data/*.xml > output.txt
+
 ketos train -f xml /home/apjanco/Downloads/hi/*.xml
 
+Errors
 train.py:438
 WARNING  No boundary given for line 
+ or 
+
+ ValueError: No valid training data was provided to the train command. Please add valid XML, line, or binary data.
+---
+
+training in eScriptorium
+https://gitlab.com/scripta/escriptorium/-/blob/develop/app/apps/core/tasks.py?ref_type=heads#L418
+
+qs = (LineTranscription.objects
+              .filter(transcription=transcription,
+                      line__document_part__pk__in=part_pks)
+              .exclude(Q(content='') | Q(content=None)))
+
+lines = E.get_document_part_lines(25,968) get baseline and mask
+image = E.get_document_part_image(25,968) get image
+# Need pathlike string, so save binary to disk and return path
+
+text = E.get_document_part_line_transcription_by_transcription(25,968 ,10637,51) #doc, part, line, transcription 
+text = text.content
+
+RecognitionModel
+https://github.com/mittagessen/kraken/blob/95981e0bcd354f37e2df7d3d07d40ebefc426400/kraken/lib/train.py#L199
+
+1.
+hyper_params: Dict[str, Any] = None,
+from kraken.lib.default_specs import RECOGNITION_HYPER_PARAMS
+hyper_params=RECOGNITION_HYPER_PARAMS,
+
+output: str = 'model',
+spec: str = default_specs.RECOGNITION_SPEC,
+append: Optional[int] = None,
+model: Optional[Union['PathLike', str]] = None,
+reorder: Union[bool, str] = True,
+2.
+training_data: Union[Sequence[Union['PathLike', str]], Sequence[Dict[str, Any]]] = None,
+training_data = [{'image': os.path.join(settings.MEDIA_ROOT, lt['image']),
+                      'text': lt['content'],
+                      'baseline': lt['baseline'],
+                      'boundary': lt['mask']} for lt in ground_truth[partition:]]
+
+3.
+evaluation_data: Optional[Union[Sequence[Union['PathLike', str]], Sequence[Dict[str, Any]]]] = None,
+evaluation_data = [{'image': os.path.join(settings.MEDIA_ROOT, lt['image']),
+                        'text': lt['content'],
+                        'baseline': lt['baseline'],
+                        'boundary': lt['mask']} for lt in ground_truth[:partition]]
+
+partition: Optional[float] = 0.9,
+binary_dataset_split: bool = False,
+num_workers: int = 1,
+load_hyper_parameters: bool = False,
+repolygonize: bool = False,
+force_binarization: bool = False,
+4.
+format_type: Literal['path', 'alto', 'page', 'xml', 'binary'] = 'path',
+codec: Optional[Dict] = None,
+resize: Literal['fail', 'both', 'new', 'add', 'union'] = 'fail')
+
+kraken_model = RecognitionModel(hyper_params=RECOGNITION_HYPER_PARAMS,
+                                    output=os.path.join(model_dir, 'version'),
+                                    # spec=spec,
+                                    # append=append,
+                                    model=load,
+                                    reorder=reorder,
+                                    format_type=None,
+                                    training_data=training_data,
+                                    evaluation_data=evaluation_data,
+                                    partition=partition,
+                                    # binary_dataset_split=fixed_splits,
+                                    num_workers=LOAD_THREADS,
+                                    load_hyper_parameters=True,
+                                    repolygonize=False,
+                                    # force_binarization=force_binarization,
+                                    # codec=codec,
+                                    resize='add')
+
+TODO experiment, run training with RecognitionModel in shell 
+
+class KrakenTrainer(pl.Trainer):
+    def __init__(self,
+                 enable_progress_bar: bool = True,
+                 enable_summary: bool = True,
+                 min_epochs: int = 5,
+                 max_epochs: int = 100,
+                 freeze_backbone=-1,
+                 pl_logger: Union[pl.loggers.logger.Logger, str, None] = None,
+                 log_dir: Optional['PathLike'] = None,
+                 *args,
+                 **kwargs):
